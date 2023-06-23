2468a2bf05f344e45345c79734cfcf58b37c96a1
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-06-24 00:58:17 +0300

    Solved a problem

---
+++ reverse-words/reverse-words/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace reverse_words;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var s = ReverseWords("a good   example");
+        System.Console.WriteLine("Hellower");
+    }
+    public static string ReverseWords(string s)
+    {
+        var reversedWords = s.Split(' ', StringSplitOptions.RemoveEmptyEntries).Reverse();
+
+        var builder = new StringBuilder();
+        foreach (var word in reversedWords)
+        {
+            builder.Append(word + " ");
+        }
+        return builder.ToString().Trim();
+    }
+}
