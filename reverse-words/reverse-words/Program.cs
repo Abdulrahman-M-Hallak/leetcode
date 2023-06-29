f9c12d83cc183f380b1a701005158e4b5ae36eca
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-06-29 19:01:24 +0300

    Refactored to use expression-bodied method

--- reverse-words/reverse-words/Program.cs
+++ reverse-words/reverse-words/Program.cs
@@ -1,16 +1,11 @@
-﻿using System.Text;
-
-namespace reverse_words;
+﻿namespace reverse_words;
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
         var s = ReverseWords("a good   example");
-        System.Console.WriteLine("Hellower");
+        System.Console.WriteLine(s);
     }
-    public static string ReverseWords(string s)
-    {
-        return string.Join(' ', s.Split(' ' , StringSplitOptions.RemoveEmptyEntries).Reverse());
-    }
+    public static string ReverseWords(string s) => string.Join(' ', s.Split(' ' , StringSplitOptions.RemoveEmptyEntries).Reverse());
 }
