998739f77e07b8841dcf4fbc399cd8c73b7b045c
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-07-14 20:27:54 +0300

    Done

---
+++ container-with-most-water/container-with-most-water/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace container_with_most_water;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        MaxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 });
+    }
+    public static int MaxArea(int[] height)
+    {
+        if (!height.Any())
+            return 0;
+
+        int i = 0, j = height.Length - 1;
+        int maxArea = j * height[j];
+        int tempArea = 0;
+        while (j > i)
+        {
+            tempArea = height[i] < height[j] ? (j - i) * height[i] : (j - i) * height[j];
+            if (tempArea > maxArea)
+                maxArea = tempArea;
+            i++; j--;
+        }
+        return maxArea;
+    }
+}
