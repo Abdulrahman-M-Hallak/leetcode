fa8c8524adde363be473e1a7a35a60f27bbd0ac2
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-07-15 20:07:40 +0300

    Done

---
+++ max-number-of-k-sum-pairs/max-number-of-k-sum-pairs/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace max_number_of_k_sum_pairs;
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine(MaxOperations(new int[] { 1, 2, 3, 4 }, 5));
+    }
+    public static int MaxOperations(int[] nums, int k)
+    {
+        Array.Sort(nums);
+        int i = 0, j = nums.Length - 1, numberOfOperation = 0;
+        while (i > j)
+        {
+            int sum = nums[i] + nums[j];
+
+            if (sum > k)
+                j--;
+            else if (sum < k)
+                i++;
+            else
+            {
+                numberOfOperation++;
+                j--; i++;
+            }
+        }
+        return numberOfOperation;
+    }
+}
