f71ce618038cef285de39343f2aaae6d6dc28625
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-07-16 13:43:35 +0300

    done

---
+++ maximum-average-array/maximum-average-array/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace maximum_average_array;
+class Program
+{
+    static void Main(string[] args)
+    {
+        FindMaxAverage(new int[] { 1, 12, -5, -6, 50, 3 }, 4);
+    }
+    public static double FindMaxAverage(int[] nums, int k)
+    {
+        double maxAverage = 0, windowSum = 0;
+
+        for (int i = 0; i < k; i++)
+            windowSum += nums[i];
+
+        maxAverage = windowSum / k;
+
+        for (int i = k; i < nums.Length; i++)
+        {
+            windowSum += nums[i] - nums[i - k];
+            if ((windowSum / k) > maxAverage)
+                maxAverage = windowSum / k;
+        }
+        return maxAverage;
+    }
+}
