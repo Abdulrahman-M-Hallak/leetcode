adf88eb40bc6bd4563fd0dbed8a61c82ca8b0705
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-07-16 20:39:26 +0300

    Done

---
+++ sliding-window/max-consecutive-ones/max-consecutive-ones/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace max_consecutive_ones;
+class Program
+{
+    static void Main(string[] args)
+    {
+        LongestOnes(new int[] { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 }, 2);
+    }
+    public static int LongestOnes(int[] nums, int k)
+    {
+        int left = 0, longestOnes = 0;
+
+        for (int right = 0; right < nums.Length; right++)
+        {
+            k += nums[right] == 0 ? -1 : 0;
+            if (k < 0)
+            {
+                k += nums[left] == 0 ? 1 : 0;
+                left++;
+            }
+            else
+            {
+                longestOnes = Math.Max(longestOnes, right - left + 1);
+            }
+        }
+        return longestOnes;
+    }
+}
