9fe547c5488f314e004c08a1477c1b5ea96353f7
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-07-22 18:42:41 +0300

    Done. With a solution that beats 99.24% of users

---
+++ prefix-sum/prefix-sum/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace prefix_sum;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    public static int LargestAltitude(int[] gain)
+    {
+        int highest = 0, sum = 0;
+        for (int i = 0; i < gain.Length; i++)
+        {
+            sum += gain[i];
+            highest = Math.Max(highest, sum);
+        }
+        return highest;
+    }
+}
