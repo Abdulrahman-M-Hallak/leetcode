75a4b8c15b8171eab7a02780cfaca803ba8df46e
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-07-26 17:52:39 +0300

    Done.

---
+++ prefix-sum/pivot-index/pivot-index/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace pivot_index;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public static int PivotIndex(int[] nums)
+    {
+        int rightSum = nums.Sum(), leftSum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            rightSum -= nums[i];
+            if (leftSum == rightSum)
+                return i;
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+}
