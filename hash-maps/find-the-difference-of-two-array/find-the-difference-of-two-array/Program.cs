9863ed85779f46c82035ebebdd0022f284c51ad5
Abdulrahman <abdulrahmanhallak04@gmail.com>
2023-08-03 00:21:17 +0300

    Done. With a solution that beats 95.11 of users

---
+++ hash-maps/find-the-difference-of-two-array/find-the-difference-of-two-array/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace find_the_difference_of_two_array;
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+    public static IList<IList<int>> FindDifference(int[] nums1, int[] nums2)
+    {
+        return new List<IList<int>>()
+        {
+            nums1.ToHashSet().Where(x => !nums2.Contains(x)).ToList(),
+            nums2.ToHashSet().Where(x => !nums1.Contains(x)).ToList()
+        };
+    }
+}
